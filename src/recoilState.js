37080657e8cc9d1bf35006f8fd001539775e30da
shubhradip <shubhradipsarkar@gmail.com>
2024-05-26 14:24:39 +0530

    recoilState added

---
+++ src/recoilState.js
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+
+export const projectsState = atom({
+    key: 'projectsState',
+    default: []
+});
+
+export const experienceState = atom({
+    key: 'experienceState',
+    default: []
+});
+
+export const descriptionState = atom({
+    key: 'descriptionState',
+    default: []
+});
