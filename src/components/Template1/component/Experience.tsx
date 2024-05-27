8107d2078e123e0f5391b000c78d402edbdf767d
shubhradip <shubhradipsarkar@gmail.com>
2024-05-27 18:58:42 +0530

    few file name changes-> JPG->jpg

--- src/components/Template1/component/Experience.tsx
+++ src/components/Template1/component/Experience.tsx
@@ -7,6 +7,7 @@ export default function Experience() {
         <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity:0, y: -100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Experience</motion.h1>
         <div>
             <div className='mb-8 flex flex-wrap lg:justify-center'>
+                
                 <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className='w-full lg:w-1/4'>
                     <p className='mb-2 text-sm text-neutral-400'>2020 - 2022</p>
                 </motion.div>
