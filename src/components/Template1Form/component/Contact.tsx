ca9efdc08db1cf0ceb5bf6af880458df4eb5072d
shubhradip <shubhradipsarkar@gmail.com>
2024-05-30 23:58:56 +0530

    feat: social links complete

--- src/components/Template1Form/component/Contact.tsx
+++ src/components/Template1Form/component/Contact.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import {motion} from 'framer-motion'
 function Contact() {
   return (
     <div className=' border-nautral-900 pb-20'>
-        <h1 className='my-10 text-center text-4xl'>Contact</h1>
+        <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity:0, y: -100}} transition={{duration:1.5}} className='my-10 text-center text-4xl'>Contact</motion.h1>
         <div className='text-center tracking-tighter'>
-            <p className='my-4 w-full text-purple-400'>Address:<input type="text" className='mr-2 pl-1 h-8 ml-2 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='xyz street'/></p>
-            <p className='my-4 w-full text-purple-400'>Email @:<input type="text" className='mr-2 pl-1 h-8 ml-2 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='xyz@abc.com'/></p>
-            <p className='my-4 w-full text-purple-400'>Contact:<input type="text" className='mr-2 pl-1 h-8 ml-2 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='+91XXXXXXXXXX'/></p>
+            <motion.p whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:1}} className='my-4 w-full text-purple-400'>Address:<input type="text" className='mr-2 pl-1 h-8 ml-2 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='xyz street'/></motion.p>
+            <motion.p whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:1.5}} className='my-4 w-full text-purple-400'>Email @:<input type="text" className='mr-2 pl-1 h-8 ml-2 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='xyz@abc.com'/></motion.p>
+            <motion.p whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:2}} className='my-4 w-full text-purple-400'>Contact:<input type="text" className='mr-2 pl-1 h-8 ml-2 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='+91XXXXXXXXXX'/></motion.p>
         </div>
     </div>
   )
