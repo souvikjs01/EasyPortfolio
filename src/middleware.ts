e61c1178526b0a4a3df773fdb72c0b97cf3e7e3e
shubhradip <shubhradipsarkar@gmail.com>
2024-06-01 22:37:51 +0530

    feat: middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse, NextRequest } from 'next/server'
+import { getToken } from "next-auth/jwt";
+
+
+export async function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname;
+    const isPublicPath = path === '/login' || path === '/register'
+    //const specialPath = path === '/joinClub'
+    //const token = request.cookies.get('token')?.value || ''
+    //console.log("the request url = ", req);
+    const token = await getToken({
+        req: req,
+        secret: process.env.NEXTAUTH_SECRET,
+    })
+
+    if(isPublicPath && token){
+        //console.log("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq",token);
+        return NextResponse.redirect(new URL('/ChooseTemplate', req.nextUrl))
+    }
+    
+    // if(!isPublicPath && !token){
+    //     return NextResponse.redirect(new URL('/login', request.nextUrl))
+    // }
+}
+ 
+export const config = {
+    matcher: [
+        '/login',
+        '/register',
+    ]
+}
