9de5dbbfbdf560b4d307b3849d16c8841a725122
shubhradip <shubhradipsarkar@gmail.com>
2024-06-01 14:07:07 +0530

    feat: go back button on templates and forms

--- src/components/Template2Form/Home2.tsx
+++ src/components/Template2Form/Home2.tsx
@@ -10,15 +10,15 @@ import Projects from "./component/Projects";
 import Contact from "../Template1Form/component/Contact";
 import SocialLinks from '../Template1Form/component/SocialLinks';
 import { useRecoilState } from 'recoil';
-import { num } from '@/recoilState';
+import { temp2Form } from '@/recoilState';
 const Home = () => {
-  const [X, setX] = useRecoilState(num);
+  const [X, setX] = useRecoilState(temp2Form);
   return (
     <div className="overflow-x-hidden text-neutral-300 bg-black">
       <div className="fixed top-0 -z-10 h-full w-full">
          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
        </div>
-       <button onClick={()=>{setX(X+1)}}>Button state {X}</button>
+       <button onClick={()=>{setX(!temp2Form)}}>{'<<< '}Go back</button>
        <div className="container mx-auto px-8">
           <Navbar />
           <About/>
