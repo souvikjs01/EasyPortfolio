6c0f2649db56be0f81c626d633572d3e0958a2c2
souvikjs01 <souvik741156@gmail.com>
2024-06-06 08:59:56 +0530

    proj&exp

--- src/components/Template2Form/component/Experience.tsx
+++ src/components/Template2Form/component/Experience.tsx
@@ -3,14 +3,25 @@ import {motion} from 'framer-motion'
 interface ExperienceItem {
     title: string;
     description: string;
+    techstacks: string[];
+    year: string;
+    company: string;
 }
 export default function Experience() {
+    const [techs, setTechs] = React.useState<string[]>([]);
     const [title, setTitle] = React.useState('');
     const [description, setdescription] = React.useState('');
+    const [tech, setTech] = React.useState('');
+    const [year, setYear] = React.useState('');
+    const [company, setCompany] = React.useState('');
     const [experience, setexperience] = React.useState<ExperienceItem[]>([]);
     const addExperience = () => {
         console.log(experience);        
-        setexperience(prevItems=>[...prevItems, {title: title, description: description}]);
+        setexperience(prevItems=>[...prevItems, {title: title, description: description, techstacks: techs, year: year, company: company}]);
+        setTechs([]);
+    }
+    const addSkill = () => {       
+        setTechs(prevItems=>[...prevItems, tech]);
     }
   return (
     <div className='border-b border-neutral-900 pb-4'>
@@ -24,9 +35,28 @@ export default function Experience() {
                         Title: <input value={title} onChange={(e)=>{setTitle(e.target.value)}} type="text" className='mb-2 ml-1 pl-1 text-sm outline outline-blue-500 outline-1 rounded-lg text-neutral-400 w-full h-8 bg-transparent' placeholder='Eg: Software Engineer, Data scientist, etc.'/>
                     </h6>
                     <p className='mb-4'>
-                        Description: <textarea value={description} onChange={(e)=>{setdescription(e.target.value)}} className='w-full ml-1 pl-1 mb-2 outline outline-blue-500 outline-1 h-32 rounded-lg text-neutral-400 bg-transparent' placeholder='Description about your work experience including company, tech stacks, duration,role etc.'></textarea>
+                        Description: <textarea value={description} onChange={(e)=>{setdescription(e.target.value)}} className='w-full ml-1 pl-1 mb-2 outline outline-blue-500 outline-1 h-32 rounded-lg text-neutral-400 bg-transparent' placeholder='Description about your work experience...'></textarea>
                     </p> 
-                    <button onClick={addExperience} className='bg-green-900 rounded-lg px-4 py-2 hover:bg-green-700'>Add Experience</button>
+                    Technologies: <input type="text" value={tech} onChange={(e)=>{setTech(e.target.value)}} className='mr-2 w-24 h-8 ml-1 pl-1 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='Eg: Kafka'/> 
+                    <button onClick={addSkill} className='mr-2 bg-purple-900 p-1 rounded-lg hover:bg-purple-500'>Add</button>
+                    <div className='flex flex-row flex-wrap mt-3'>
+                        
+                        {techs.map((key, index)=>(
+                            
+                            <span className='flex items-center mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-600'>{key}</span>
+                            
+                        ))}
+                        
+                    
+                    </div>
+                    <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className='w-full lg:w-1/4'>
+                        Year: <input value={year} onChange={(e)=>{setYear(e.target.value)}} className='mb-2 text-sm outline outline-blue-500 outline-1 rounded-lg text-neutral-400 ml-1 pl-1 w-28 h-8 bg-transparent' placeholder='Eg: 2020-2021'/>
+                    </motion.div>
+                    <h6 className='mb-2 font-semibold'>
+                        Company: <input value={company} onChange={(e)=>{setCompany(e.target.value)}} type="text" className='mb-2 ml-1 pl-1 text-sm outline outline-blue-500 outline-1 rounded-lg text-neutral-400 w-48 h-8 bg-transparent' placeholder='Eg: Amazon'/>
+                    </h6>
+
+                    <button onClick={addExperience} className='bg-green-900 rounded-lg px-4 py-2 mt-2 hover:bg-green-700'>Add Experience</button>
                 </motion.div>
             </div>
             {/* {
--- src/components/Template2Form/component/Projects.tsx
+++ src/components/Template2Form/component/Projects.tsx
@@ -3,20 +3,27 @@ import Image from 'next/image'
 import {motion} from 'framer-motion'
 
 interface ProjectItems {
-    title: string;
-    desc: string;
-    url: string;
+    projectName: string;
+    description: string;
+    technologies: string[];
+    github: string;
+    hosted: string;
 }
 function Projects() {
-    const [title, setTitle] = React.useState('');
-    const [desc, setDesc] = React.useState('');
-    const [url, setUrl] = React.useState('');
-    const [project, setProject] = React.useState<ProjectItems[]>([]);
-    const addProject = () => {
-        setProject(prevProj => [...prevProj, {title: title, desc: desc, url: url}]);
-        console.log(project);        
+    const [projectName, setprojectName] = useState('');
+    const [description, setdescription] = useState('');
+    const [technologies, settechnologies] = useState<string[]>([]);
+    const [tech, settech] = useState('');
+    const [github, setgithub] = useState('');
+    const [hosted, sethosted] = useState('');
+    const [projects, setProjects] = useState<ProjectItems[]>([]);
+    
+    const addTechnology = () => {
+        settechnologies(prevItem => [...prevItem, tech]);
     }
-
+    const addProject = () => {
+        setProjects(prevItem => [...prevItem, {projectName: projectName, description: description, technologies: technologies, github: github, hosted: hosted}]);
+    }
   return (
     <div className='border-b border-neutral-900 pb-4'>
         <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity:0, y: -100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Projects</motion.h1>
@@ -26,16 +33,26 @@ function Projects() {
                     <Image src="/o1.jpg" alt="" width={200} height={200} className='rounded-lg'/>
                 </div>
                 <div className='w-full max-w-xl lg:w-3/4'>
-                    <h6 className='mb-2 font-semibold'>Project Title: <input onChange={(e) => {
-                        setTitle(e.target.value)
+                    <h6 className='mb-2 font-semibold'>Project Name: <input onChange={(e) => {
+                        setprojectName(e.target.value)
                     }} type="text" className='mb-2 text-sm outline outline-blue-500 outline-1 rounded-lg text-neutral-400 ml-1 pl-1 w-56 h-8 bg-transparent' placeholder='Eg: Tic-Tac-Toe'/></h6>
                     <p className='mb-4 text-neutral-400'><textarea onChange={(e) => {
-                        setDesc(e.target.value)
+                        setdescription(e.target.value)
                     }} className='w-full ml-1 pl-1 mb-2 outline outline-blue-500 outline-1 h-32 rounded-lg text-neutral-400 bg-transparent' placeholder='Description about the project...'/></p>
-                    Project-URL: <input onChange={(e) => {
-                        setUrl(e.target.value)
-                    }} type="link"  className='mr-2 w-full h-8 ml-1 pl-1 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='Eg: https://xyz-project-vercel.com'/>
-                    <button onClick={addProject} className='mt-2 mr-2 bg-purple-900 px-4 py-2 rounded-lg hover:bg-purple-500'>Add</button>
+                    Technologies - <input type="text" value={tech} onChange={(e)=>{settech(e.target.value)}}  className='mr-2 w-24 h-8 ml-1 pl-1 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent' placeholder='Eg: Kafka'/> <button onClick={addTechnology}  className='mr-2 bg-purple-900 p-1 rounded-lg hover:bg-purple-500'>Add</button>
+                    
+                    <div className='mt-2 flex flex-row flex-wrap'>
+                        {technologies.map((techi)=>(
+                            <div className='mt-1 mr-1'>
+                                <span className=' rounded bg-neutral-900  px-2 py-1 text-sm font-medium text-purple-600'>{techi}</span>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='mt-2'>
+                        Github Link - <input type="text" name="" value={github} onChange={(e)=>{setgithub(e.target.value)}} className='mr-2 w-full lg:w-24 h-8 ml-1 pl-1 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent'/>
+                        Hosted Link - <input type="text" name="" value={hosted} onChange={(e)=>{sethosted(e.target.value)}} className='mr-2 w-full lg:w-24 h-8 ml-1 pl-1 outline outline-blue-500 outline-1 rounded-lg text-neutral-400 bg-transparent'/>
+                    </div>
+                    <button onClick={addProject}  className='bg-green-900 p-3 my-2 mx-2 rounded-lg hover:bg-green-700'>Add Project</button>
                     
                 </div>
             </motion.div>
