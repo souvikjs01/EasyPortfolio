c8d347ba7e3b6963ea5080e0ad8cfb47e3e0d749
souvikjs01 <souvik741156@gmail.com>
2024-06-09 09:18:59 +0530

    ui-1

---
+++ src/app/ui/page.tsx
@@ -0,0 +1,12 @@
+import PurpleUi from '@/components/ui/PurpleUi'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <PurpleUi />
+    </div>
+  )
+}
+
+export default page
